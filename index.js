d406978faab673ab7f404db8cdb04aa83b580e27
enquete-motivationjeunes <163045282+enquete-motivationjeunes@users.noreply.github.com>
2024-03-26 20:39:57 +0100

    Update liens

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-const urlList = ["https://questionnaires.univ-nantes.fr/index.php/348637?lang=fr","https://questionnaires.univ-nantes.fr/index.php/286916?lang=fr", "https://questionnaires.univ-nantes.fr/index.php/367774?lang=fr", "https://questionnaires.univ-nantes.fr/index.php/971457?lang=fr","https://questionnaires.univ-nantes.fr/index.php/987974?lang=fr","https://questionnaires.univ-nantes.fr/index.php/672313?lang=fr","https://questionnaires.univ-nantes.fr/index.php/534431?lang=fr","https://questionnaires.univ-nantes.fr/index.php/368784?lang=fr","https://questionnaires.univ-nantes.fr/index.php/427521?lang=fr","https://questionnaires.univ-nantes.fr/index.php/276696?lang=fr","https://questionnaires.univ-nantes.fr/index.php/216469?lang=fr","https://questionnaires.univ-nantes.fr/index.php/712362?lang=fr","https://questionnaires.univ-nantes.fr/index.php/116971?lang=fr","https://questionnaires.univ-nantes.fr/index.php/139818?lang=fr","https://questionnaires.univ-nantes.fr/index.php/542533?lang=fr","https://questionnaires.univ-nantes.fr/index.php/449668?lang=fr","https://questionnaires.univ-nantes.fr/index.php/596628?lang=fr","https://questionnaires.univ-nantes.fr/index.php/542733?lang=fr","https://questionnaires.univ-nantes.fr/index.php/731234?lang=fr","https://questionnaires.univ-nantes.fr/index.php/221385?lang=fr"];
+const urlList = [
+"https://questionnaires.univ-nantes.fr/index.php/498582?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/677514?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/996942?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/168527?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/813863?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/137533?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/861516?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/236718?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/337158?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/152841?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/925117?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/235667?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/354337?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/732353?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/289557?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/199492?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/874991?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/776112?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/748538?lang=fr", 
+"https://questionnaires.univ-nantes.fr/index.php/221385?lang=fr"
+];
 
 window.location.href = urlList[Math.floor(Math.random() * (urlList.length -1))]
 
